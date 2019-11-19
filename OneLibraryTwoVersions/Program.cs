463a253fca35225116ddd07d9bacc5bb5dc2a9ae
Orace <pierre.lando@gmail.com>
2019-11-19 17:21:00 +0100

    Added OneLibraryTwoVersions hack

---
+++ OneLibraryTwoVersions/Program.cs
@@ -0,0 +1,23 @@
+﻿extern alias NLog3;
+extern alias NLog4;
+using System;
+
+namespace OneLibraryTwoVersions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date1 = new NLog3::NLog.LogFactory();
+            var date2 = new NLog4::NLog.LogFactory();
+
+            Console.WriteLine(date1.GetType().Assembly.FullName);
+            Console.WriteLine(date1.GetType().Assembly.CodeBase);
+            Console.WriteLine();
+            Console.WriteLine(date2.GetType().Assembly.FullName);
+            Console.WriteLine(date2.GetType().Assembly.CodeBase);
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hacks.NET
 Hacks for the .NET
+
+
+## OneLibraryTwoVersions
+How to compile and run a project that contains two version of the same library
