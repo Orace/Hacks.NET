9d894d4a55aaeddf8fd194a287af183484880944
Orace <pierre.lando@gmail.com>
2019-11-19 17:45:01 +0100

    Add AddGitCommitHashInAssemblyInformationalVersion hack.

---
+++ AddGitCommitHashInAssemblyInformationalVersion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace AddGitCommitHashInAssemblyInformationalVersion
+{
+    class Program
+    {
+        static void Main()
+        {
+            var informationalVersion = typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            Console.Write("Informational version is: " + informationalVersion ?? "<null>");
+            Console.ReadKey();
+        }
+    }
+}
